71c5b0bfe52cbdc947a7bdf7af005b767664b488
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-15 02:14:33 +0900

    remove useless import lines from manage.py

--- manage.py
+++ manage.py
@@ -15,7 +15,6 @@ if os.path.exists('.env'):
 
 from app import create_app
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
